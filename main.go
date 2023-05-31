bdadabab0b510a2c2ff0e2ff0112e0298cb3dfd4
mamadou <amadou.mbodj@ynov.com>
2023-05-31 17:03:13 +0200

    server

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package Forum
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Index)
+
+	port := ":3000"
+	fmt.Printf("Serveur en cours d'exécution sur le port %s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
