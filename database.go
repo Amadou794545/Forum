4014302f7b4b5d47fa2fca2220b668386a1a9dd3
karl Dambreville <karldamb27@gmail.com>
2023-06-02 16:48:38 +0200

    checkEmail + checkUsername

--- database.go
+++ database.go
@@ -285,3 +285,29 @@ func updateUsername(username string, id_user int) {
 		log.Fatal(err)
 	}
 }
+
+func checkUsername(pseudo string) bool {
+	rows, err := db.Query("SELECT pseudo FROM Posts WHERE pseudo = $1", pseudo)
+	if err != nil {
+		return false
+	}
+	defer rows.Close()
+	if rows.Next() {
+		return true // exist
+	}
+
+	return false // does not exist
+}
+
+func checkEmail(email string) bool {
+	rows, err := db.Query("SELECT email FROM Posts WHERE email = $1", email)
+	if err != nil {
+		return false
+	}
+	defer rows.Close()
+	if rows.Next() {
+		return true // exist
+	}
+
+	return false // does not exist
+}
