fa05cae70b706be42abc0cde7f7d35e9dbf596f9
mamadou <amadou.mbodj@ynov.com>
2023-06-05 17:27:57 +0200

    Post

--- java-script/post.js
+++ java-script/post.js
@@ -2,28 +2,38 @@ function post() {
     var description = document.getElementById("description").value;
     var titre = document.getElementById("titre").value;
 
-    var Container = document.getElementById("container");
+    var descriptionContainer = document.getElementById("container");
 
     var newDiv = document.createElement("div");
     var newTitre = document.createElement("div");
     var newDescription = document.createElement("div");
 
+    newDiv.classList.add("post-item");
     newTitre.textContent = "Titre : " + titre;
     newDescription.textContent = "Description : " + description;
 
-    // Ajouter une classe CSS spécifique au titre
-    newTitre.classList.add("titre-style");
-    newDiv.classList.add("div-item");
-
     newDiv.appendChild(newTitre);
     newDiv.appendChild(newDescription);
-    Container.appendChild(newDiv);
-    Container.insertAdjacentHTML("beforeend", "<br>");
 
+    descriptionContainer.appendChild(newDiv);
 
     document.getElementById("description").value = "";
     document.getElementById("titre").value = "";
 }
+
+document.getElementById("imageInput").addEventListener("change", function(event) {
+    var file = event.target.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            var imageUrl = reader.result;
+            var imageElement = document.createElement("img");
+            imageElement.src = imageUrl;
+            document.getElementById("container").appendChild(imageElement);
+        };
+        reader.readAsDataURL(file);
+    }
+});
 
 function toggleDiv() {
     var div = document.getElementById("myDiv");
