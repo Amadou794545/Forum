c75585e2e0c3a2cfddf4cdbc4dd806a02c9494d1
Revoky <fannycr1708@gmail.com>
2023-06-05 17:34:08 +0200

    database

--- Database/checks.go
+++ Database/checks.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 func checkUsername(pseudo string) bool {
 	rows, err := db.Query("SELECT pseudo FROM Users WHERE pseudo = $1", pseudo)
 	if err != nil {
@@ -24,4 +26,14 @@ func checkEmail(email string) bool {
 	}
 
 	return false // does not exist
+}
+
+func checkLogin(identifier string, password string) bool {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM Users WHERE (email = ? OR pseudo = ?) AND password = ?", identifier, identifier, password).Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return count > 0
 }
