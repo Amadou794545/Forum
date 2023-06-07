f06a550d7a623eefd4a816dc7bf5196e9b0b92f9
Revoky <fannycr1708@gmail.com>
2023-06-07 17:27:30 +0200

    sessionCookie

---
+++ handlers.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"forum/Database"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		if CheckSessionCookie(r) {
+			cookie, err := r.Cookie("session")
+			if err != nil {
+				// Handle the error if needed
+				fmt.Println("Error retrieving session cookie:", err)
+				return
+			}
+
+			userID := cookie.Value
+			username, err := Database.GetUserUsername(userID)
+			if err != nil {
+				// Handle the error if needed
+				fmt.Println("Error retrieving username:", err)
+				return
+			}
+
+			fmt.Println("Bienvenue", username)
+		}
+
+		http.ServeFile(w, r, "template/index.html")
+	}
+}
+
+func handlerInscription(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/inscription" {
+		if r.Method == "GET" {
+			http.ServeFile(w, r, "template/inscription.html")
+		} else if r.Method == "POST" {
+			// Récupérer les données du formulaire d'inscription (username, email, password)
+			username := r.FormValue("username")
+			email := r.FormValue("email")
+			password := r.FormValue("password")
+			// Vérifier si le mot de passe contient au moins un chiffre, une majuscule et un caractère spécial
+			hasDigit := regexp.MustCompile(`\d`).MatchString(password)
+			hasUpper := strings.ToUpper(password) != password
+			hasSpecial := regexp.MustCompile(`[^a-zA-Z0-9]`).MatchString(password)
+
+			if !hasDigit || !hasUpper || !hasSpecial {
+				errorMessage := "Le mot de passe doit contenir au moins un chiffre, une majuscule et un caractère spécial"
+				http.Error(w, errorMessage, http.StatusBadRequest)
+				http.ServeFile(w, r, "template/inscription.html")
+				return
+			} else {
+				Database.AddUser(email, username, password, "test")
+				http.Redirect(w, r, "/", http.StatusFound)
+			}
+
+			// Afficher les données d'inscription dans la console
+			fmt.Println("Nouvel utilisateur enregistré :")
+			fmt.Println("Username :", username)
+			fmt.Println("Email :", email)
+			fmt.Println("Password :", password)
+
+			// Enregistrer l'utilisateur dans la base de données
+
+			// Rediriger vers la page de connexion ou afficher un message de succès
+		}
+
+	}
+}
+
+func handlerConnexion(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/login" {
+		if r.Method == "GET" {
+			http.ServeFile(w, r, "template/login.html")
+		} else if r.Method == "POST" {
+
+			// Vérification des informations de connexion
+			username := r.FormValue("username")
+			password := r.FormValue("password")
+
+			if username == "Harry" && password == "passw0rd." {
+				fmt.Println("ok log")
+				userID, err := Database.GetUserID(username)
+				if err != nil {
+					// Handle the error appropriately
+					fmt.Println("Error:", err)
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+				HandlerCookie(w, r, userID) // Ajout du cookie de session
+				//UpdateSessionExpiration(w, r) // Mise à jour de sa durée
+				http.Redirect(w, r, "/", http.StatusFound)
+				return
+			}
+
+			fmt.Println("ko")
+			http.ServeFile(w, r, "template/login.html")
+		}
+	}
+}
--- server.go
+++ server.go
@@ -3,93 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"regexp"
-	"strings"
-
-	"forum/Database"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	//page
-	http.HandleFunc("/login", Connexion)
-	http.HandleFunc("/inscription", Inscription)
-	http.HandleFunc("/", Index)
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/inscription", handlerInscription)
+	http.HandleFunc("/login", handlerConnexion)
 
-	//css
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
 
-	//server
 	port := ":3030"
 	fmt.Printf("Serveur en cours d'exécution sur le port %s\n", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		fmt.Println("Erreur :", err)
 	}
-}
-
-func Connexion(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path == "/login" {
-		if r.Method == "GET" {
-			http.ServeFile(w, r, "template/login.html")
-		} else if r.Method == "POST" {
-			// Récupérer les données du formulaire de connexion (username, password)
-			username := r.FormValue("username")
-			password := r.FormValue("password")
-
-			// Vérifier les informations de connexion
-
-			if Database.CheckLogin(username, password) {
-				println("ok log")
-				http.Redirect(w, r, "/", http.StatusFound)
-			} else {
-				println("ko")
-				http.ServeFile(w, r, "template/login.html")
-			}
-		}
-	}
-}
-
-func Inscription(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/inscription" {
-		if r.Method == "GET" {
-			http.ServeFile(w, r, "template/inscription.html")
-		} else if r.Method == "POST" {
-			// Récupérer les données du formulaire d'inscription (username, email, password)
-			username := r.FormValue("username")
-			email := r.FormValue("email")
-			password := r.FormValue("password")
-			// Vérifier si le mot de passe contient au moins un chiffre, une majuscule et un caractère spécial
-			hasDigit := regexp.MustCompile(`\d`).MatchString(password)
-			hasUpper := strings.ToUpper(password) != password
-			hasSpecial := regexp.MustCompile(`[^a-zA-Z0-9]`).MatchString(password)
-
-			if !hasDigit || !hasUpper || !hasSpecial {
-				errorMessage := "Le mot de passe doit contenir au moins un chiffre, une majuscule et un caractère spécial"
-				http.Error(w, errorMessage, http.StatusBadRequest)
-				http.ServeFile(w, r, "template/inscription.html")
-				return
-			} else {
-				Database.AddUser(email, username, password, "test")
-				http.Redirect(w, r, "/", http.StatusFound)
-			}
-
-			// Afficher les données d'inscription dans la console
-			fmt.Println("Nouvel utilisateur enregistré :")
-			fmt.Println("Username :", username)
-			fmt.Println("Email :", email)
-			fmt.Println("Password :", password)
-
-			// Enregistrer l'utilisateur dans la base de données
-
-			// Rediriger vers la page de connexion ou afficher un message de succès
-		}
-
-	}
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "template/index.html")
 }
