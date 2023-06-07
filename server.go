9679d4b9e7fb5774e558dc4e4b67fca421fb668d
karl Dambreville <karldamb27@gmail.com>
2023-06-07 14:59:22 +0200

    added error message if bad login info

--- server.go
+++ server.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-	"regexp"
-	"strings"
 
 	"forum/Database"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type LoginData struct {
+	Username     string
+	ErrorMessage string
+}
 
 func main() {
 	//page
@@ -19,6 +23,10 @@ func main() {
 
 	//css
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
+
+	//js
+
+	http.Handle("/java-script/", http.StripPrefix("/java-script", http.FileServer(http.Dir("java-script"))))
 
 	//server
 	port := ":3030"
@@ -30,64 +38,53 @@ func main() {
 }
 
 func Connexion(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./template/login.html"))
+	loginData := LoginData{
+		Username:     "",
+		ErrorMessage: "",
+	}
 
-	if r.URL.Path == "/login" {
-		if r.Method == "GET" {
-			http.ServeFile(w, r, "template/login.html")
-		} else if r.Method == "POST" {
-			// Récupérer les données du formulaire de connexion (username, password)
-			username := r.FormValue("username")
-			password := r.FormValue("password")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	println(username)
+	println(password)
+	if username != "" && password != "" { //a retirer quand premier check ok en js
+		if Database.CheckLogin(username, password) {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
 
-			// Vérifier les informations de connexion
+			errorMessage := "Nom d'utilisateur ou Mot de passe invalide"
 
-			if Database.CheckLogin(username, password) {
-				println("ok log")
-				http.Redirect(w, r, "/", http.StatusFound)
-			} else {
-				println("ko")
-				http.ServeFile(w, r, "template/login.html")
-			}
+			loginData.Username = username
+			loginData.ErrorMessage = errorMessage
 		}
 	}
+	tmpl.Execute(w, loginData)
 }
 
 func Inscription(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/inscription" {
-		if r.Method == "GET" {
-			http.ServeFile(w, r, "template/inscription.html")
-		} else if r.Method == "POST" {
-			// Récupérer les données du formulaire d'inscription (username, email, password)
-			username := r.FormValue("username")
-			email := r.FormValue("email")
-			password := r.FormValue("password")
-			// Vérifier si le mot de passe contient au moins un chiffre, une majuscule et un caractère spécial
-			hasDigit := regexp.MustCompile(`\d`).MatchString(password)
-			hasUpper := strings.ToUpper(password) != password
-			hasSpecial := regexp.MustCompile(`[^a-zA-Z0-9]`).MatchString(password)
+	if r.Method == "GET" {
+		http.ServeFile(w, r, "template/inscription.html")
+	} else if r.Method == "POST" {
+		// Récupérer les données du formulaire d'inscription (username, email, password)
+		username := r.FormValue("username")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
 
-			if !hasDigit || !hasUpper || !hasSpecial {
-				errorMessage := "Le mot de passe doit contenir au moins un chiffre, une majuscule et un caractère spécial"
-				http.Error(w, errorMessage, http.StatusBadRequest)
-				http.ServeFile(w, r, "template/inscription.html")
-				return
-			} else {
-				Database.AddUser(email, username, password, "test")
-				http.Redirect(w, r, "/", http.StatusFound)
-			}
+		Database.AddUser(email, username, password, "test")
+		http.Redirect(w, r, "/", http.StatusFound)
 
-			// Afficher les données d'inscription dans la console
-			fmt.Println("Nouvel utilisateur enregistré :")
-			fmt.Println("Username :", username)
-			fmt.Println("Email :", email)
-			fmt.Println("Password :", password)
+		// Afficher les données d'inscription dans la console
+		fmt.Println("Nouvel utilisateur enregistré :")
+		fmt.Println("Username :", username)
+		fmt.Println("Email :", email)
+		fmt.Println("Password :", password)
 
-			// Enregistrer l'utilisateur dans la base de données
+		// Enregistrer l'utilisateur dans la base de données
 
-			// Rediriger vers la page de connexion ou afficher un message de succès
-		}
-
+		// Rediriger vers la page de connexion ou afficher un message de succès
 	}
+
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
