1e78f07cd825d4a19bd81cc91e838dc430c1b808
Revoky <fannycr1708@gmail.com>
2023-06-21 10:36:29 +0200

    css_docker

--- server.go
+++ server.go
@@ -31,10 +31,8 @@ func main() {
 	http.HandleFunc("/api/posts", GetPostsAPI)
 	http.HandleFunc("/api/user/posts", GetUserPostsAPI)
 
-	//voir si les modifs suivantes rajoutent le css au serveur quand il tourne via docker
-	//fs := http.FileServer(http.Dir("/go/bin/CSS"))
-	//http.Handle("/CSS/", http.StripPrefix("/CSS/", fs))
-	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css"))))
+	fs := http.FileServer(http.Dir("/go/bin/CSS"))
+	http.Handle("/CSS/", http.StripPrefix("/CSS/", fs))
 	http.Handle("/java-script/", http.StripPrefix("/java-script", http.FileServer(http.Dir("java-script"))))
 
 	http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir("uploads"))))
